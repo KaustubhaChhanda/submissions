b64ce2021642ea597593a473705892ad132d7148
Kaustubha Chhanda <kaustubhachhandapersonal@gmail.com>
2026-08-14 20:38:55 +0530

    Time: 5 ms (5.61%), Space: 51.7 MB (5.02%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        Integer[][] memo = new Integer[prices.length][2];
+        return dfs(prices, 0, 0, memo);
+    }
+
+    private int dfs(int[] prices, int index, int isHolding, Integer[][] memo) {
+        if (index == prices.length) {
+            return 0;
+        }
+
+        if (memo[index][isHolding] != null) {
+            return memo[index][isHolding];
+        }
+
+        int skip = dfs(prices, index + 1, isHolding, memo);
+        int action = 0;
+
+        if (isHolding == 0) {
+            action = -prices[index] + dfs(prices, index + 1, 1, memo);
+        } else {
+            action = prices[index] + dfs(prices, index + 1, 0, memo);
+        }
+
+        return memo[index][isHolding] = Math.max(skip, action);
+    }
+}
