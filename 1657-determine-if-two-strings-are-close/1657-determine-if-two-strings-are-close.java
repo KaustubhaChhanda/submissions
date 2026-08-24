db201e972e16101fa9ccf9f489705b9ed4c95f5c
Kaustubha Chhanda <kaustubhachhandapersonal@gmail.com>
2026-08-24 19:15:23 +0530

    Time: 34 ms (35.96%), Space: 48.1 MB (36.04%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        Set<Character> set = new HashSet<>();
+        int[] f1 = new int[26];
+        int[] f2 = new int[26];
+
+        for (char ch : word1.toCharArray()) {
+            set.add(ch);
+            f1[ch - 'a']++;
+        }
+
+        for (char ch : word2.toCharArray()) {
+            if (!set.contains(ch)) {
+                return false;
+            }
+            
+            f2[ch - 'a']++;
+        }
+
+        Arrays.sort(f1);
+        Arrays.sort(f2);
+
+        for (int i = 0; i < 26; i++) {
+            if (f1[i] != f2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
