0049fb6027d2dbea86275579912dc6d5b2fd21a5
Kaustubha Chhanda <kaustubhachhandapersonal@gmail.com>
2026-09-05 22:33:43 +0530

    Time: 0 ms (100%), Space: 46.5 MB (31.67%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -0,0 +1,20 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int low = 0, high = letters.length - 1;
+        int index = 0;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (letters[mid] == target) {
+                low = mid + 1;
+            } else if (letters[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return (low < letters.length) ? letters[low] : letters[0];
+    }
+}
