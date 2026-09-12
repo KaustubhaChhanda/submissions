4619bbc371f76f26cf8a23a05777d695703e7e00
Kaustubha Chhanda <kaustubhachhandapersonal@gmail.com>
2026-09-12 20:50:08 +0530

    Time: 6 ms (92.16%), Space: 48 MB (35.35%) - LeetHub

---
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxDistinct(String s) {
+        int[] freq = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        int count = 0;
+
+        for (int f : freq) {
+            if (f != 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
