56d02fedc3e817b4f5eaf89a2384ef2f9062daa0
Kaustubha Chhanda <kaustubhachhandapersonal@gmail.com>
2026-09-13 14:24:08 +0530

    Time: 2 ms (100%), Space: 46.5 MB (61.45%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -6,44 +6,64 @@ class Solution {
             char ch = s.charAt(i);
             char prev = i > 0 ? s.charAt(i - 1) : '0';
 
-            if (ch == 'I') {
-                res += 1;
-            } else if (ch == 'V') {
-                if (prev == 'I') {
-                    res += 3;
-                } else {
-                    res += 5;
-                }
-            } else if (ch == 'X') {
-                if (prev == 'I') {
-                    res += 8;
-                } else {
-                    res += 10;
-                }
-            } else if (ch == 'L') {
-                if (prev == 'X') {
-                    res += 30;
-                } else {
-                    res += 50;
-                }
-            } else if (ch == 'C') {
-                if (prev == 'X') {
-                    res += 80;
-                } else {
-                    res += 100;
-                }
-            } else if (ch == 'D') {
-                if (prev == 'C') {
-                    res += 300;
-                } else {
-                    res += 500;
-                }
-            } else {
-                if (prev == 'C') {
-                    res += 800;
-                } else {
-                    res += 1000;
-                }
+            switch (ch) {
+                case 'I':
+                    res += 1;
+                    break;
+
+                case 'V':
+                    if (prev == 'I') {
+                        res += 3;
+                    } else {
+                        res += 5;
+                    }
+
+                    break;
+
+                case 'X':
+                    if (prev == 'I') {
+                        res += 8;
+                    } else {
+                        res += 10;
+                    }
+
+                    break;
+
+                case 'L':
+                    if (prev == 'X') {
+                        res += 30;
+                    } else {
+                        res += 50;
+                    }
+
+                    break;
+
+                case 'C':
+                    if (prev == 'X') {
+                        res += 80;
+                    } else {
+                        res += 100;
+                    }
+
+                    break;
+
+                case 'D':
+                    if (prev == 'C') {
+                        res += 300;
+                    } else {
+                        res += 500;
+                    }
+
+                    break;
+
+                case 'M':
+                    if (prev == 'C') {
+                        res += 800;
+                    } else {
+                        res += 1000;
+                    }
+                    
+                    break;
             }
         }
 
